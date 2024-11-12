6e5287e03b3721939b72fe25187f29b98b739662
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-11-12 23:38:51 +0200

    update: add readme + changed input encoding

--- libs/operations.js
+++ libs/operations.js
@@ -2,14 +2,14 @@ import crypto from "node:crypto";
 
 export const encrypt = (text, key, iv) => {
   const cipher = crypto.createCipheriv("aes-128-ctr", key, iv);
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
+  let encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
   return encrypted;
 };
 
 export const decrypt = (encryptedText, key, iv) => {
   const decipher = crypto.createDecipheriv("aes-128-ctr", key, iv);
-  let decrypted = decipher.update(encryptedText, "hex", "utf8");
+  let decrypted = decipher.update(encryptedText, "base64", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
 };
